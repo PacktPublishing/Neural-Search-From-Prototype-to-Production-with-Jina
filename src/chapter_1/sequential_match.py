45470a03592a23bdc814add975228f0dce522f1c
bwanglzu <bo.wang@jina.ai>
2021-09-02 15:53:39 +0200

    feat: add chapter 1 script 1

---
+++ src/chapter_1/sequential_match.py
@@ -0,0 +1,19 @@
+import os
+import glob
+
+
+def match_sequentially():
+    query = 'hello jina'
+    matches = []
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    txt_files = glob.glob(f'{dir_path}/resources/*.txt')
+    for txt_file in txt_files:
+        with open(txt_file, 'r') as f:
+            if query in f.read():
+                matches.append(txt_file)
+    return matches
+
+
+if __name__ == '__main__':
+    matches = match_sequentially()
+    print(matches)
